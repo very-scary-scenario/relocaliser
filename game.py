4a8d2d617c3e644e7de283f8070d3e4376fc5319
colons <code@colons.co>
2017-01-20 10:48:41 +0000

    Recover when things go bad.
    Closes #3

--- game.py
+++ game.py
@@ -1,19 +1,37 @@
 import signal
 
+from camel import CamelRegistry
 from Levenshtein import ratio
 
 from party import interesting_party
 from text import normalise
 
 
+camel_registry = CamelRegistry()
+
+
 class Game:
-    def __init__(self):
-        self.steps = interesting_party()
+    def __init__(self, steps=None):
+        if steps is None:
+            self.steps = interesting_party()
+        else:
+            self.steps = steps
+
         self.original = self.steps[0][-1]
         self.clue = self.steps[-1][-1]
 
     def play(self, guess):
         return ratio(normalise(guess), normalise(self.original))
+
+
+@camel_registry.dumper(Game, 'game', version=1)
+def _dump_game(game):
+    return {'steps': game.steps}
+
+
+@camel_registry.loader('game', version=1)
+def _load_game(data, version):
+    return Game(**data)
 
 
 if __name__ == '__main__':
--- twitter.py
+++ twitter.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
 import html
+import os
 
+from camel import Camel
 import tweepy
 
-from game import Game
+from game import Game, camel_registry
 import keys
 
 
@@ -13,14 +15,33 @@ api = tweepy.API(auth)
 
 handle = auth.get_username()
 
+GAMES_DIR = os.path.join(os.path.dirname(__file__), 'games')
+if not os.path.isdir(GAMES_DIR):
+    os.mkdir(GAMES_DIR)
+
+camel = Camel([camel_registry])
+
 
 class TwitterGame(tweepy.StreamListener):
-    def __init__(self, api, game, initial_status_id):
+    def __init__(
+        self, api, game, initial_status_id, trigger_status_ids=None,
+        end_at=None, over=False,
+    ):
         self.game = game
         self.initial_status_id = initial_status_id
-        self.trigger_status_ids = {initial_status_id}
-        self.end_at = datetime.now() + timedelta(hours=1)
+        self.trigger_status_ids = [initial_status_id]
+
+        if end_at is None:
+            end_at = datetime.now() + timedelta(hours=1)
+
+        self.end_at = end_at
+        self.over = over
+        self.save()
         return super().__init__(api)
+
+    def save(self):
+        with open(os.path.join(GAMES_DIR, self.end_at.isoformat()), 'w') as sf:
+            sf.write(camel.dump(self))
 
     def handle_play(self, status):
         if not status.text.startswith('@{}'.format(handle)):
@@ -31,7 +52,7 @@ class TwitterGame(tweepy.StreamListener):
             # ignore things that aren't replies to the game in progress
             return
 
-        self.trigger_status_ids.add(status.id)
+        self.trigger_status_ids.append(status.id)
 
         text = html.unescape(status.text)
         entry = text.replace('@{}'.format(handle), '').strip()
@@ -43,14 +64,15 @@ class TwitterGame(tweepy.StreamListener):
                 "@{} Correct! I'll start a new game soon.".format(
                     status.author.screen_name, score,
                 ),
-                in_reply_to_status_id=status.id,
+                n_reply_to_status_id=status.id,
             )
             api.retweet(status.id)
+            self.over = True
 
             return False
         else:
             # provide feeback
-            self.trigger_status_ids.add(api.update_status(
+            self.trigger_status_ids.append(api.update_status(
                 "@{} That's {:.1%} right.".format(
                     status.author.screen_name, score,
                 ),
@@ -62,6 +84,7 @@ class TwitterGame(tweepy.StreamListener):
 
         if rv is False:
             # there's no need to force the game to end; it's already over
+            self.save()
             return rv
 
         elif datetime.now() > self.end_at:
@@ -69,17 +92,57 @@ class TwitterGame(tweepy.StreamListener):
                 'Game over. The answer was {}.'.format(self.game.original),
                 in_reply_to_status_id=self.initial_status_id,
             )
+            self.over = True
+            self.save()
             return False
 
         else:
+            self.save()
             return rv
+
+    def on_connect(self):
+        mentions = api.mentions_timeline(since_id=self.trigger_status_ids[-1])
+        for status in sorted(mentions, key=lambda s: s.created_at):
+            if self.on_status(status) is False:
+                raise ValueError('this game ended while we were away')
+
+
+def start_new_game():
+    game = Game()
+    listener = TwitterGame(api, game, api.update_status(game.clue).id)
+    return listener
 
 
 def run_game():
-    game = Game()
-    listener = TwitterGame(api, game, api.update_status(game.clue).id)
+    saved_games = sorted(
+        (fn for fn in os.listdir(GAMES_DIR) if not fn.startswith('.'))
+    )
+
+    if not saved_games:
+        listener = start_new_game()
+    else:
+        with open(os.path.join(GAMES_DIR, saved_games[-1])) as gf:
+            listener = camel.load(gf.read())
+        if listener.over:
+            listener = start_new_game()
+
     stream = tweepy.Stream(auth=api.auth, listener=listener)
     stream.userstream()
+
+
+@camel_registry.dumper(TwitterGame, 'twitter_game', version=1)
+def _dump_twitter_game(tg):
+    return {
+        a: getattr(tg, a) for a in [
+            'game', 'initial_status_id', 'trigger_status_ids', 'end_at',
+            'over',
+        ]
+    }
+
+
+@camel_registry.loader('twitter_game', version=1)
+def _load_twitter_game(data, version):
+    return TwitterGame(api=api, **data)
 
 
 if __name__ == '__main__':
