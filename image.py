c3497886c94d8c144a0360cff23605feb79b6998
colons <colons@colons.co>
2022-02-20 14:43:56 +0000

    Get some kind of very rudimentary text rendering to work.

--- image.py
+++ image.py
@@ -12,9 +12,14 @@ WIDTH = 800
 STEP_SIZE = 100
 VERTICAL_MARGIN = 175
 
+HERE = os.path.realpath(os.path.join(__file__, '..'))
+
 
 def _generate_image(driver: WebDriver, steps: List[Tuple[Language, str]], filename: str) -> None:
     driver.set_window_size(WIDTH, STEP_SIZE * len(steps) + VERTICAL_MARGIN)
+    driver.get(f'file:{os.path.join(HERE, "summary.html")}')
+    driver.execute_script("""ingestSteps(["hello", "there"])""")
+
     with open(filename, 'wb') as png:
         png.write(driver.get_screenshot_as_png())
 
